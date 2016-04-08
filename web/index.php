4b62e7f8d1e662d9b973c5db53a632e00c7eaf36
rafaelglikis <rafaelglikis@gmail.com>
2016-04-08 16:59:55 +0300

    Index Page

--- web/index.php
+++ web/index.php
@@ -2,10 +2,18 @@
 <html>
 <head>
     <title>The HashTag Finder</title>
+    <link rel="stylesheet" type="text/css" href="../vendor/twbs/bootstrap/dist/css/bootstrap.css">
 </head>
 <body>
 
 <h1>The HashTag Finder</h1>
+
+<div class="container">
+    <div class="jumbotron">
+        <h1>The HashTag Finder</h1>
+        <p>A Tool that process a given url and retrieves <strong>#hashtags<strong> from its content.</p>
+    </div>
+</div>
 
 </body>
 </html>
