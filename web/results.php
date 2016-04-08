9656c88e62ad3834e7a344dac3ba648787ad775b
rafaelglikis <rafaelglikis@gmail.com>
2016-04-09 00:04:50 +0300

    Index Page

--- web/results.php
+++ web/results.php
@@ -14,12 +14,10 @@ $sourse = new Url($_POST["url"])
 <div class="container">
     <div class="row">
         <div class="col-xs-12">
-            <!--
             <h2><?php echo $sourse->getTitle()?></h2>
             <p>Url: <?php echo $sourse->getUrl()?></p>
             <img src="<?php echo $sourse->getImage()?>"
                  alt="<?php echo $sourse->getTitle()?>">
-            -->
         </div>
     </div>
 </div>
