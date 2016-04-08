5905f4efeb499a0ae3deac3207da4bdec2b3efdd
rafaelglikis <rafaelglikis@gmail.com>
2016-04-08 17:05:03 +0300

    Index Page

--- web/index.php
+++ web/index.php
@@ -5,6 +5,8 @@
     <link rel="stylesheet" type="text/css" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
 
