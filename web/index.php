fa72448e1307a64259945e29673a2bbce2117938
rafaelglikis <rafaelglikis@gmail.com>
2016-04-08 16:46:26 +0300

    getting started

--- web/index.php
+++ web/index.php
@@ -1,5 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>The HashTag Finder</title>
+</head>
+<body>
+
+<h1>The HashTag Finder</h1>
+
+</body>
+</html>
+
 <?php
 
 require('../vendor/autoload.php');
 
-echo "Hellow World";
+?>
+
