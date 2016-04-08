905bc17242df77bf00ef694a10a340e1143f35bc
rafaelglikis <rafaelglikis@gmail.com>
2016-04-08 17:02:58 +0300

    Index Page

--- web/index.php
+++ web/index.php
@@ -3,6 +3,8 @@
 <head>
     <title>The HashTag Finder</title>
     <link rel="stylesheet" type="text/css" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
 <body>
 
@@ -11,7 +13,7 @@
 <div class="container">
     <div class="jumbotron">
         <h1>The HashTag Finder</h1>
-        <p>A Tool that process a given url and retrieves <strong>#hashtags<strong> from its content.</p>
+        <p>A Tool that process a given url and retrieves <strong>#hashtags</strong> from its content.</p>
     </div>
 </div>
 
