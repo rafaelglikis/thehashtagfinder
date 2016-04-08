3034e10e5eaf0c6d571276b2b1d8e52fde628695
rafaelglikis <rafaelglikis@gmail.com>
2016-04-08 17:54:38 +0300

    Index Page

--- web/index.php
+++ web/index.php
@@ -9,9 +9,12 @@ require('../vendor/autoload.php');
     <title>The HashTag Finder</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrab -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="../web/stylesheets/mystyles.css">
 </head>
 <body>
 <div class="container">
