da503077afa06b8779690a128e15c30ddb1feba2
rafaelglikis <rafaelglikis@gmail.com>
2016-04-08 23:07:23 +0300

    Index Page

--- web/views/form.php
+++ web/views/form.php
@@ -9,7 +9,7 @@
                     <h2>Give me the link!</h2>
                     <div class="input-group col-md-12">
                         <input type="url" oninvalid="setCustomValidity('Enter a valid url bro!')"
-                               class="form-control input-lg" placeholder="www.example.com/blabla/" name="url" id="focusedInput" required/>
+                               class="form-control input-lg" placeholder="http://www.example.com/blabla/" name="url" id="focusedInput" required/>
 
                         <span class="input-group-btn">
                             <button class="btn btn-info btn-lg" type="submit">
