b736c16eb7c4c52c0c248317ef6622b92ebfa517
rafaelglikis <rafaelglikis@gmail.com>
2016-04-08 17:24:41 +0300

    Index Page

--- web/index.php
+++ web/index.php
@@ -3,31 +3,6 @@
 require('../vendor/autoload.php');
 
 ?>
-<!--
-<!DOCTYPE html>
-<html>
-<head>
-    <title>The HashTag Finder</title>
-    <link rel="stylesheet" type="text/css" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
-<body>
-
-<h1>The HashTag Finder</h1>
-
-<div class="container">
-    <div class="jumbotron">
-        <h1>The HashTag Finder</h1>
-        <p>A Tool that process a given url and retrieves <strong>#hashtags</strong> from its content.</p>
-    </div>
-</div>
-
-</body>
-</html>
--->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -66,4 +41,28 @@ require('../vendor/autoload.php');
 
 </body>
 </html>
+<!--
+<!DOCTYPE html>
+<html>
+<head>
+    <title>The HashTag Finder</title>
+    <link rel="stylesheet" type="text/css" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
 
+<h1>The HashTag Finder</h1>
+
+<div class="container">
+    <div class="jumbotron">
+        <h1>The HashTag Finder</h1>
+        <p>A Tool that process a given url and retrieves <strong>#hashtags</strong> from its content.</p>
+    </div>
+</div>
+
+</body>
+</html>
+-->
