07362a02f402eaa811d054ea6e111cdc30d15291
rafaelglikis <rafaelglikis@gmail.com>
2016-04-08 21:06:04 +0300

    Index Page

--- web/index.php
+++ web/index.php
@@ -55,11 +55,11 @@ require('../vendor/autoload.php');
             <div class="col-xs-12 col-md-4">
                 <p>Developed by <a href="http://rafael.glikis.net/">Rafael Glikis</a> for <a href="http://nextweb.ninja/">Web Ninja</a> contest.</p>
             </div>
-            <div class="col-xs-12 col-md-4">
-                <a href="http://www.facebook.com/rafael.glikis"> <i class="fa fa-facebook" id="custom-footer-icon"></i></a>
-                <a href="http://www.linkedin.com/in/rafael-glikis-b4361a80?trk=hp-identity-name"><i class="fa fa-linkedin"></i></a>
-                <a href="http://github.com/rafaelglikis"> <i class="fa fa-github"></i></a>
-                <a href="mailto:rafaelglikis@gmail.com"><i class="fa fa-envelope"></i></a>
+            <div class="col-xs-12 col-md-4 col-md-offset-4">
+                <a href="http://www.facebook.com/rafael.glikis"> <i class="fa fa-facebook custom-footer-icon"></i></a>
+                <a href="http://www.linkedin.com/in/rafael-glikis-b4361a80?trk=hp-identity-name custom-footer-icon"><i class="fa fa-linkedin"></i></a>
+                <a href="http://github.com/rafaelglikis"> <i class="fa fa-github custom-footer-icon"></i></a>
+                <a href="mailto:rafaelglikis@gmail.com"><i class="fa fa-envelope custom-footer-icon"></i></a>
             </div>
         </div>
     </div>
