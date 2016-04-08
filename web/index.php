cbadc398f433178eced237d58601c0643244963b
rafaelglikis <rafaelglikis@gmail.com>
2016-04-08 18:55:15 +0300

    Index Page

--- web/index.php
+++ web/index.php
@@ -13,6 +13,8 @@ require('../vendor/autoload.php');
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <!-- FontAwesome -->
+    <link rel="stylesheet" type="text/css" href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css">
 
     <link rel="stylesheet" href="../web/stylesheets/mystyles.css">
 </head>
@@ -45,6 +47,53 @@ require('../vendor/autoload.php');
         </div>
     </div>
 </div>
+
+<footer>
+    <section id="footer" style="background-color:#292b34;">
+        <div class="container">
+            <center style="color:#fff;">
+                <div class="container">
+                    <div class="well well-sm main-footer"  style="background-color:#292b34; border:0px">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <h1 class="title-footer">
+                                            Quick Contact</h1>
+                                    </div><!--end .col-md-4-->
+                                    <div class="col-md-4 contact-email">
+                                        <h2>
+                                            <a href="mailto:nico@nicoplyley.com">nico@nicoplyley.com</a></h2>
+                                        <h3>
+                                            © Copyright Nico Plyley 2014</h3>
+                                    </div><!--end .col-md-4 .contact-email-->
+                                    <div class="col-md-4">
+                                        <div class="row contact row-first">
+                                            <div class="col-md-6">
+                                                <a href="skype:nico.plyley?chat" class="skype"><i class="fa fa-skype"></i>nico.plyley</a>
+                                            </div><!--end .col-md-6-->
+                                            <div class="col-md-6">
+                                                <a href="http://instagram.com/nicoplyley" class="instagram"><i class="fa fa-instagram"></i>@nicoplyley</a>
+                                            </div><!--end .col-md-6-->
+                                        </div><!--end .row .contact .row-first-->
+                                        <div class="row contact">
+                                            <div class="col-md-6">
+                                                <a href="http://facebook.com/nicoplyley" class="facebook"><i class="fa fa-facebook"></i>@nicoplyley</a>
+                                            </div><!--end .col-md-6-->
+                                            <div class="col-md-6">
+                                                <a href="http://twitter.com/nicoplyley" class="twitter"><i class="fa fa-twitter"></i>@nicoplyley</a>
+                                            </div><!--end .col-md-6-->
+                                        </div><!--end .row .contact-->
+                                    </div><!--end .col-md-6-->
+                                </div><!--end .row-->
+                            </div><!--end .col-md-12-->
+                        </div><!--end .row-->
+                    </div><!--end .well .well-sm .main-footer-->
+                </div><!--end .container-->
+            </center>
+        </div><!--end .container-->
+    </section><!--end #footer-->
+</footer>
 </body>
 </html>
 
