e007d41afea5424dcb8603f7cdd9126fddf7956a
rafaelglikis <rafaelglikis@gmail.com>
2016-04-09 04:04:03 +0300

    Index Page

--- web/results.php
+++ web/results.php
@@ -19,17 +19,17 @@ $sourse = new Url($_POST["url"])
 <div class="container">
     <div class="row">
         <div class="col-xs-12">
-            <div id="whatever">
-                <a href="/path" rel="7">peace</a>
-                <a href="/path" rel="3">unity</a>
-                <a href="/path" rel="10">love</a>
-                <a href="/path" rel="5">having fun</a>
-            </div>
             <h2><?php echo $sourse->getTitle()?></h2>
             <p>Url: <?php echo $sourse->getUrl()?></p>
             <img src="<?php echo $sourse->getImage()?>"
                  alt="<?php echo $sourse->getTitle()?>">
             <p>
+                <div id="whatever">
+                    <a href="#" rel="7">peace</a>
+                    <a href="#" rel="3">unity</a>
+                    <a href="#" rel="10">love</a>
+                    <a href="#" rel="5">having fun</a>
+                </div>
                 <?php
                 $hashTags = $sourse->getHashTags();
                 foreach ($hashTags as $hashTag)
@@ -41,7 +41,7 @@ $sourse = new Url($_POST["url"])
                 <script>
                     $.fn.tagcloud.defaults = {
                         size: {start: 14, end: 18, unit: 'pt'},
-                        color: {start: '#cde', end: '#f52'}
+                        color: {start: '#eee', end: '#337}
                     };
 
                     $(function () {
