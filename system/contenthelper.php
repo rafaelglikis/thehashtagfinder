324720383bb4625db7d49940fd09e66c31ad2f61
rafaelglikis <rafaelglikis@gmail.com>
2016-04-09 16:57:38 +0300

    images stop caring

--- system/contenthelper.php
+++ system/contenthelper.php
@@ -1,15 +1,20 @@
 <?php
+include('init.php');
 class ContentHelper
 {
 
 
-static function calculateKeyWordsWeight($keyWords)
+static function calculateKeyWordsWeight($keyWords, $url)
     {
         $uniqueKeyWords = array_unique ($keyWords);
         $uniqueKeyWordCounts = array_count_values ($keyWords);
 
         arsort($uniqueKeyWordCounts);
         //var_dump($uniqueKeyWordCounts);
+
+        //Majestic
+        $majesticKeywords = ContentHelper::getMajecticBacklinks($url);
+        var_dump($majesticKeywords);
 
         $hashTags  = array();
 
@@ -38,12 +43,8 @@ static function calculateKeyWordsWeight($keyWords)
         $keyWords = explode(" ", $content);
         $keyWords = preg_replace('/[0-9]+/', '', $keyWords);
         $keyWords = array_filter($keyWords);
-
-        //Majestic
-        $majesticKeywords = ContentHelper::getMajecticBacklinks('http://www.subtitles.gr/');
-        var_dump($majesticKeywords);
         
-        $newKeyWords = ContentHelper::calculateKeyWordsWeight($keyWords);
+        $newKeyWords = ContentHelper::calculateKeyWordsWeight($keyWords, $url);
         
         return $newKeyWords;
     }
@@ -65,10 +66,10 @@ static function calculateKeyWordsWeight($keyWords)
         //open connection
         $ch = curl_init();
 
-        //read cookie from file for security
-        //$cookie = file_get_contents("majecticcookie");
-        //var_dump($cookie);
-        $cookie = "RURI=account%2Flogin; _ga=GA1.2.1123763203.1459339548; _pk_id.2.c4b1=99ba1936f88898b3.1459339549.2.1460204509.1460203189.; visitor_id63022=179076790; _pk_ses.2.c4b1=*; STOK=naeL7yiM689kSIKAOM4f70u2qT; _gat=1";
+        //read cookie from init,php
+        global $majesticCookie;
+        $cookie = $majesticCookie;
+
         //set the url, number of POST vars, POST data
         curl_setopt($ch,CURLOPT_URL, "https://majestic.com/data-output");
         curl_setopt($ch,CURLOPT_POST, count($fields_string));
