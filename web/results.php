09d34cdac988a2747f84f790dabb54aac46620b6
rafaelglikis <rafaelglikis@gmail.com>
2016-04-09 18:56:25 +0300

    design

--- web/results.php
+++ web/results.php
@@ -97,7 +97,7 @@ $sourse = new Url($_POST["url"])
                 }
             </script>
             <script type="text/javascript" src="js/canvasjs.min.js"></script>
-            <div id="chartContainer" style="height: 500px; width: 100%;"></div>
+            <div class="col-xs-12 col-md-6 col-md-offset-3" id="chartContainer" style="height: 500px; width: 100%;"></div>
         </div>
     </div>
 
