bc0a723c26cc8922b9616428248d6496bdac74aa
rafaelglikis <rafaelglikis@gmail.com>
2016-04-09 16:45:32 +0300

    images stop caring

--- system/contenthelper.php
+++ system/contenthelper.php
@@ -66,8 +66,9 @@ static function calculateKeyWordsWeight($keyWords)
         $ch = curl_init();
 
         //read cookie from file for security
-        $cookie = file_get_contents("majecticcookie");
-        var_dump($cookie);
+        //$cookie = file_get_contents("majecticcookie");
+        //var_dump($cookie);
+        $cookie = "RURI=account%2Flogin; _ga=GA1.2.1123763203.1459339548; _pk_id.2.c4b1=99ba1936f88898b3.1459339549.2.1460204509.1460203189.; visitor_id63022=179076790; _pk_ses.2.c4b1=*; STOK=naeL7yiM689kSIKAOM4f70u2qT; _gat=1"
         //set the url, number of POST vars, POST data
         curl_setopt($ch,CURLOPT_URL, "https://majestic.com/data-output");
         curl_setopt($ch,CURLOPT_POST, count($fields_string));
