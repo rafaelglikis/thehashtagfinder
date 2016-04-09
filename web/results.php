e973cb52e75177eb42887f266a507477f0502a89
rafaelglikis <rafaelglikis@gmail.com>
2016-04-09 14:06:38 +0300

    more design

--- web/results.php
+++ web/results.php
@@ -18,6 +18,7 @@ $sourse = new Url($_POST["url"])
 
 <div class="container">
     <div class="row">
+        <p>Url: <?php echo $sourse->getUrl()?></p>
         <div class="col-xs-12">
             <div class="panel-group" id="accordion"
                  role="tablist" aria-multiselectable="true">
@@ -56,64 +57,60 @@ $sourse = new Url($_POST["url"])
                         </div>
                     </div>
                     <div class="panel panel-default">
-                        <div class="panel-heading" role="tab" id="headingAgumbe">
+                        <div class="panel-heading" role="tab" id="headingTagCloud">
                             <h3 class="panel-title">
                                 <a class="collapsed" role="button" data-toggle="collapse"
-                                   data-parent="#accordion" href="#agumbe"
-                                   aria-expanded="false" aria-controls="agumbe">
-                                    Agumbe Tang <small>Chief Taste Officer</small></a>
+                                   data-parent="#accordion" href="#tagcloud"
+                                   aria-expanded="false" aria-controls="tagcloud">
+                                    TagCloud <small>Shows the main words of url content as tag cloud.</small></a>
                             </h3>
                         </div>
                         <div role="tabpanel" class="panel-collapse collapse"
-                             id="agumbe"    aria-labelledby="headingAgumbe">
+                             id="tagcloud"    aria-labelledby="headingAgumbe">
                             <div class="panel-body">
-                                <p>Blessed with the most discerning gustatory sense, Agumbe, our CFO, personally ensures that every dish that we serve meets his exacting tastes. Our chefs dread the tongue lashing that ensues if their dish does not meet his exacting standards. He lives by his motto, <em>You click only if you survive my lick.</em></p>
+                                <div id="whatever">
+                                    <p>
+                                        <?php
+                                        $hashTags = $sourse->getHashTags();
+                                        foreach ($hashTags as $hashTag)
+                                        {
+                                            ?>
+                                            <a href="#" rel="<?php echo $hashTag->getWeight()*1000 ?>">
+                                                <?php echo $hashTag->getName() ?></a>
+                                            <?php
+                                        }
+                                        ?>
+                                        <script>
+                                            $.fn.tagcloud.defaults = {
+                                                size: {start: 10, end: 26, unit: 'pt'},
+                                                color: {start: '#CDE', end: '#F52'}
+                                            };
+                                            $(function () {
+                                                $('#whatever a').tagcloud();
+                                            });
+                                        </script>
+                                    </p>
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div class="panel panel-default">
-                        <div class="panel-heading" role="tab" id="headingAlberto">
+                        <div class="panel-heading" role="tab" id="headingHashTags">
                             <h3 class="panel-title">
                                 <a class="collapsed" role="button" data-toggle="collapse"
-                                   data-parent="#accordion" href="#alberto"
-                                   aria-expanded="false" aria-controls="alberto">
-                                    Alberto Somayya <small>Executive Chef</small></a>
+                                   data-parent="#accordion" href="#hashTags"
+                                   aria-expanded="false" aria-controls="hashTags">
+                                    #hashtags <small>Top 10 keywords</small></a>
                             </h3>
                         </div>
                         <div role="tabpanel" class="panel-collapse collapse"
-                             id="alberto"    aria-labelledby="headingAlberto">
+                             id="hashTags"    aria-labelledby="headingHashTags">
                             <div class="panel-body">
-                                <p>Award winning three-star Michelin chef with wide International experience having worked closely with whos-who in the culinary world, he specializes in creating mouthwatering Indo-Italian fusion experiences. He says, <em>Put together the cuisines from the two craziest cultures, and you get a winning hit! Amma Mia!</em></p>
+                                <p></p>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <h2></h2>
-            <p>Url: <?php echo $sourse->getUrl()?></p>
-
-            <div id="whatever">
-                <p>
-                    <?php
-                    $hashTags = $sourse->getHashTags();
-                    foreach ($hashTags as $hashTag)
-                    {
-                        ?>
-                        <a href="#" rel="<?php echo $hashTag->getWeight()*1000 ?>">
-                            <?php echo $hashTag->getName() ?></a>
-                        <?php
-                    }
-                    ?>
-                    <script>
-                        $.fn.tagcloud.defaults = {
-                            size: {start: 10, end: 26, unit: 'pt'},
-                            color: {start: '#CDE', end: '#F52'}
-                        };
-                        $(function () {
-                            $('#whatever a').tagcloud();
-                        });
-                    </script>
-                </p>
             </div>
         </div>
     </div>
