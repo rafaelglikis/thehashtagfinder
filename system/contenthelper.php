026d2cddd6a919e4d04a2043a65ac8c3b33345b1
rafaelglikis <rafaelglikis@gmail.com>
2016-04-10 01:33:29 +0300

    adding links

--- system/contenthelper.php
+++ system/contenthelper.php
@@ -163,10 +163,10 @@ class ContentHelper
         curl_close($ch);
 
         //clearing non-alphanumerics
-        /*foreach($keywords as &$keyword)
+        foreach($keywords as &$keyword)
         {
             $keyword = preg_replace("/[^A-Za-z0-9. ]/", '', $keyword);
-        }*/
+        }
         return $keywords;
     }
     
