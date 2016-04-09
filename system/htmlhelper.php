0dbc6f61522ff9ee07fa64ee9da2eef179828527
rafaelglikis <rafaelglikis@gmail.com>
2016-04-09 15:45:58 +0300

    image opt

--- system/htmlhelper.php
+++ system/htmlhelper.php
@@ -80,7 +80,7 @@ class HtmlHelper
             $html = preg_replace('#<script(.*?)>(.*?)</script>#is', '', $html);
             $html = preg_replace('#<style(.*?)>(.*?)</style>#is', '', $html);
             $image = TextHelper::strCut($html, '<img src=">','/>');
-            var_dump($html);
+            var_dump($image);
         }
         //return $image;
     }
