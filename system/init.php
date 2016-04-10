a8016ba41e3c237bff3cfe40f4e86fb9d45e4e51
rafaelglikis <rafaelglikis@gmail.com>
2016-04-10 21:58:03 +0300

    cookie

--- system/init.php
+++ system/init.php
@@ -1,2 +1,2 @@
 <?php
-$majesticCookie = "RURI=account%2Flogin; _ga=GA1.2.1123763203.1459339548; _pk_id.2.c4b1=99ba1936f88898b3.1459339549.2.1460204509.1460203189.; visitor_id63022=179076790; _pk_ses.2.c4b1=*; STOK=naeL7yiM689kSIKAOM4f70u2qT; _gat=1" ; // Your majestic cookie here
+$majesticCookie = getenv('MAJESTIC_COOKIE'); // Your majestic cookie here
