532ff0399cc69d15dded50acbe0bcb4697743726
rafaelglikis <rafaelglikis@gmail.com>
2016-04-10 07:15:04 +0300

    debug

--- web/results.php
+++ web/results.php
@@ -1,8 +1,9 @@
 <?php
 // Error Display
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
+
 // Composer
 require('../vendor/autoload.php');
 
