bed9c739afd51290b378afbc47e3812307e71542
rafaelglikis <rafaelglikis@gmail.com>
2016-04-10 15:50:49 +0300

    chart

--- web/results.php
+++ web/results.php
@@ -79,6 +79,7 @@ $sourse = new Url($_POST["url"])
 
                         <div id="canvas-holder">
                             <center>
+                                <p> Hashtag weights </p>
                                 <canvas id="chart-area" width="500" height="500"/>
                             </center>
                         </div>
